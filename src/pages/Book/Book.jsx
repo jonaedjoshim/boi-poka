97924ac815c30436797729b2259ce77fcc3a57de
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2026-01-08 20:13:57 +0600

    tenth commit

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -6,7 +6,7 @@ const Book = ({ singleBook }) => {
   const { bookName, category, image, publisher, rating, tags } = singleBook;
 
   return (
-    <div className="p-6 m-6 border border-[#13131390] rounded-2xl">
+    <div className="p-6 m-6 border border-[#13131390] rounded-2xl shadow-lg">
       <div>
         <figure className="bg-[#F3F3F3] rounded-2xl px-18 py-16 mb-5">
           <img src={image} alt="Book" className="h-72 mx-auto" />
@@ -15,9 +15,9 @@ const Book = ({ singleBook }) => {
       <div>
         <div className="border-b border-dashed mb-5 pb-5 text-left space-y-5">
           <div className="flex flex-wrap gap-3">
-            {tags.map((tag) => (
+            {tags.map((tag,index) => (
               <div
-                key={tag}
+                key={index}
                 className="badge badge-soft badge-success text-[#23BE0A] px-4 py-3"
               >
                 {tag}
