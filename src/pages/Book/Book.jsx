4535ad0904a91702c4e12121ca73d113d5feb802
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2026-01-08 19:55:50 +0600

    ninth commit

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,33 +1,37 @@
 import React from "react";
+import { FaStar } from "react-icons/fa";
 
 const Book = ({ singleBook }) => {
   console.log(singleBook);
-  const { author, bookName, category, image, publisher, rating, review } =
-    singleBook;
+  const { bookName, category, image, publisher, rating, tags } = singleBook;
 
   return (
     <div className="p-6 m-6 border border-[#13131390] rounded-2xl">
       <div>
-        <figure className="bg-[#F3F3F3] rounded-2xl px-20 py-16 mb-5">
-          <img src={image} alt="Book" className="h-96 mx-auto" />
+        <figure className="bg-[#F3F3F3] rounded-2xl px-18 py-16 mb-5">
+          <img src={image} alt="Book" className="h-72 mx-auto" />
         </figure>
       </div>
       <div>
         <div className="border-b border-dashed mb-5 pb-5 text-left space-y-5">
-          <div className="space-x-3">
-            <div className="badge badge-soft badge-success text-[#23BE0A] px-4 py-2">
-              adult
-            </div>
-            <div className="badge badge-soft badge-success text-[#23BE0A] px-4 py-2">
-              identity
-            </div>
+          <div className="flex flex-wrap gap-3">
+            {tags.map((tag) => (
+              <div
+                key={tag}
+                className="badge badge-soft badge-success text-[#23BE0A] px-4 py-3"
+              >
+                {tag}
+              </div>
+            ))}
           </div>
           <h2 className="text-2xl font-bold">{bookName}</h2>
           <p className="text-base font-medium">By : {publisher}</p>
         </div>
         <div className="flex justify-between">
           <p>Category : {category}</p>
-          <div>Rating : {rating}</div>
+          <div className="flex items-center gap-2">
+            Rating : {rating} <FaStar />
+          </div>
         </div>
       </div>
     </div>
