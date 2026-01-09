937cb9f78c17c0e36f9b76963e126d7db7d985f2
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2026-01-09 17:30:21 +0600

    14th commit

--- src/pages/readList/readList.jsx
+++ src/pages/readList/readList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useLoaderData } from "react-router";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
+import { IoIosArrowDropdown } from "react-icons/io";
 import { getStoredBook } from "../../Utility/addToDB";
 import Book from "../Book/Book";
 
@@ -18,24 +19,49 @@ const readList = () => {
   }, [data]);
 
   return (
-    <Tabs>
-      <TabList>
-        <Tab>My Read List</Tab>
-        <Tab>My Wishlst</Tab>
-      </TabList>
+    <div className="mt-6">
+      <div className="font-bold text-3xl bg-[#131313] rounded-2xl py-4 flex justify-center">
+        <h1>Books</h1>
+      </div>
+      <button
+        className="btn flex items-center justify-around font-semibold bg-[#23BE0A] mx-auto my-8"
+        popoverTarget="popover-1"
+        style={{ anchorName: "--anchor-1" }}
+      >
+        Sort By <IoIosArrowDropdown className="text-base mt-1" />
+      </button>
 
-      <TabPanel>
-        <h2>read list books {readList.length}</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          {readList.map((book) => (
-            <Book key={book.bookId} singleBook={book}></Book>
-          ))}
-        </div>
-      </TabPanel>
-      <TabPanel>
-        <h2>wish list books</h2>
-      </TabPanel>
-    </Tabs>
+      <ul
+        className="dropdown menu w-52 rounded-box bg-base-100 shadow-sm"
+        popover="auto"
+        id="popover-1"
+        style={{ positionAnchor: "--anchor-1" }}
+      >
+        <li>
+          <a>Item 1</a>
+        </li>
+        <li>
+          <a>Item 2</a>
+        </li>
+      </ul>
+      <Tabs>
+        <TabList>
+          <Tab>My Read List</Tab>
+          <Tab>My Wishlst</Tab>
+        </TabList>
+        <TabPanel>
+          <h2>read list books {readList.length}</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            {readList.map((book) => (
+              <Book key={book.bookId} singleBook={book}></Book>
+            ))}
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <h2>wish list books</h2>
+        </TabPanel>
+      </Tabs>
+    </div>
   );
 };
 
